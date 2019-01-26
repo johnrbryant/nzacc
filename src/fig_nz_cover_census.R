cdcfe7441b60868955e8943c66fc56abd533446c
johnrbryant <john.r.bryant@gmail.com>
2019-01-26 18:38:45 +1300

    tidying graphs

--- src/fig_nz_cover_census.R
+++ src/fig_nz_cover_census.R
@@ -8,23 +8,32 @@ library(dplyr)
 coverage <- fetchCoverage("out/model.est",
                           dataset = "census")
 
-p <- dplot(~ age | factor(time) * sex,
+p <- dplot(~ age | factor(time),
            data = coverage,
+           subarray = sex == "Male",
            col = "grey",
            xlab = "Year",
            as.table = TRUE,
            scales = list(tck = 0.4),
            ylab = "",
            midpoints = "age",
+           prob = c(0.025, 0.5, 0.975),
            par.settings = list(fontsize = list(text = 7),
                                strip.background = list(col = "grey90")),
-           between = list(y = 0.2)) %>%
-    useOuterStrips() +
-    layer(panel.abline(h = 1, lty = "dotted"))
+           between = list(x = 0.2, y = 0.2)) +
+    layer(panel.abline(h = 1, col = "black", lwd = 0.5),
+          under = TRUE)
 
 graphics.off()
-pdf(file = "out/fig_cover_census.pdf",
+pdf(file = "out/fig_nz_cover_census.pdf",
     width = 5,
-    height = 3.5)
+    height = 3)
 plot(p)
 dev.off()
+
+
+
+
+
+
+
